45d4e6c072e6c1bc1eac669d0a376217cdad823c
RICARDO LUCAS TIEPPO MARTINS <105604399+RTieppo@users.noreply.github.com>
2023-07-02 15:50:23 -0300

    primeiros ajuste
    interações iniciais e ajustes de interação.

--- play_musica/__main__.py
+++ play_musica/__main__.py
@@ -1,7 +1,58 @@
+from PySimpleGUI import PySimpleGUI as sg
+
 from funcoes_extras import tela_play as tela
 
 
 
+def start():
+    try:
+        aleatorio_button = play_button = loop_button = True
+
+        tela_inicial = tela.tela_main()
+
+        while True:
+            window, events, values = sg.read_all_windows()
+
+            if window == tela_inicial and events == sg.WIN_CLOSED:
+                break
+
+            elif window == tela_inicial and events == '-aleatorio-':
+                if aleatorio_button == True:
+                    window['-aleatorio-'].update(r'play_musica/img/play/aleatorio_25_cor.png')
+                    aleatorio_button = False
+                
+                else:
+                    window['-aleatorio-'].update(r'play_musica\img\play\aleatorio_25.png')
+                    aleatorio_button = True
+            
+            elif window == tela_inicial and events == '-volta-':
+                print('volta')
+
+            elif window == tela_inicial and events == '-play-pause-':
+                if play_button == True:
+                    window['-play-pause-'].update(r'play_musica\img\play\pausa_25.png')
+                    play_button = False
+                
+                else:
+                    window['-play-pause-'].update(r'play_musica\img\play\botao-play_25.png')
+                    play_button = True
+
+            elif window == tela_inicial and events == '-proximo-':
+                print('next')
+
+            elif window == tela_inicial and events == '-loop-':
+
+                if loop_button == True:
+                    window['-loop-'].update(r'play_musica\img\play\replay_25_cor.png')
+                    loop_button = False
+                
+                else:
+                    window['-loop-'].update(r'play_musica\img\play\replay_25.png')
+                    loop_button = True
+
+    except Exception as erro:
+        print(erro)
+
 
 if __name__ == '__main__':
-    pass
+    inicia = start()
--- play_musica/funcoes_extras/tela_play/__init__.py
+++ play_musica/funcoes_extras/tela_play/__init__.py
@@ -7,6 +7,35 @@ tema_g = 'DarkBlue2'
 #Define fonte
 
 
-
 def tela_main():
-    pass
+    
+    sg.theme(tema_g)
+
+    display =[
+        [sg.Image(r'play_musica\img\test\pylot.png')]
+    ]
+
+    cabeca = [
+        [sg.Text('PySimpleGUI Player - By RTieppo', justification='c')],
+        [sg.Frame('',layout=display)],
+        [sg.Text('Aperte o Play...',justification='c')]
+
+    ]
+
+    corpo = [
+        [sg.Image(r'play_musica\img\play\aleatorio_25.png',pad=(20,10), enable_events=True,key='-aleatorio-'),sg.Image(r'play_musica\img\play\costas_25.png',pad=(20,10),enable_events=True,key='-volta-'), sg.Image(r'play_musica\img\play\botao-play_25.png',pad=(20,10),enable_events=True,key='-play-pause-'), sg.Image(r'play_musica\img\play\proximo_25.png',pad=(20,10),enable_events=True,key='-proximo-'), sg.Image(r'play_musica\img\play\replay_25.png',pad=(20,10),enable_events=True,key='-loop-')],
+    ]
+
+    pe =[
+
+    ]
+
+    main =[
+        [sg.Column(layout=cabeca,element_justification='c')],
+        [sg.HSep()],
+        [sg.Column(layout=corpo,element_justification='c',pad=(20,20))]
+    ]
+
+    return sg.Window('MIX.PY', finalize=True, size=(400,550), layout = main,
+    margins=(10,10), element_justification='c', icon= (r'play_musica\img\icon\botoes.ico'),
+    text_justification='c')
