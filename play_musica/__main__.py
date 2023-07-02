13ba1e7a918262868fc01ae7c313d7a1222080e1
RICARDO LUCAS TIEPPO MARTINS <105604399+RTieppo@users.noreply.github.com>
2023-07-02 14:21:36 -0300

    test

--- play_musica/__main__.py
+++ play_musica/__main__.py
@@ -1,5 +1,7 @@
 from funcoes_extras import tela_play as tela
 
 
+
+
 if __name__ == '__main__':
     pass
--- play_musica/funcoes_extras/tela_play/__init__.py
+++ play_musica/funcoes_extras/tela_play/__init__.py
@@ -2,8 +2,10 @@
 from PySimpleGUI import PySimpleGUI as sg
 
 #Definindo tema geral
-
 tema_g = 'DarkBlue2'
+
+#Define fonte
+
 
 
 def tela_main():
